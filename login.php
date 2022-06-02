e4299e3c77ae1693c0810f5f85dee8817e14ccef
JizhuoChen <88676760+JizhuoChen@users.noreply.github.com>
2022-06-02 19:37:39 +0800

    Create login.php

---
+++ login.php
@@ -0,0 +1,36 @@
+<?php
+header('Content-Type: text/html; charset=utf-8');
+$username1 = $_POST['username'];
+$userPwd = $_POST['userPwd'];
+//database information:
+$servername = "localhost";
+$username = "root";
+$password = "root";
+$dbname = "test";
+//connect to database:
+$conn = new mysqli($servername,$username,$password,$dbname);
+//check connection：
+if ($conn->connect_error){
+    die("connection failed".$conn->connect_error);
+}
+//check with database：
+$sql = "select userPwd from user where username='".$username1."'";
+$allSql = "select * from user where username='".$username1."'";
+$result = $conn->query($sql);
+$allResult = $conn->query($allSql);
+if ($username1 == "" or $userPwd == ""){
+    echo '<script>alert("Username or password cannot be empty");history.go(-1);</script>';
+}
+else if($result->num_rows > 0){
+    $row = $result->fetch_row();
+    $db_userpwd = $row[0];
+    if($db_userpwd==$userPwd){
+        echo 'Welcome'.$username1.'Login';
+    }else{
+        echo '<script>alert("Wrong username or password"); history.go(-1);</script>';
+    }
+}else {
+    echo '<script>alert("Username does not exist"); history.go(-1);</script>';
+}
+$conn->close();
+?>
